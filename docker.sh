e373c8859e1f9dc8f33d768e608bbc19a4903a74
Andy Osborne <caosborne.az@gmail.com>
2020-12-05 16:14:45 -0700

    Clean up code comments

--- docker.sh
+++ docker.sh
@@ -5,20 +5,10 @@
 #
 #         USAGE: ./docker.sh
 #
-#   DESCRIPTION: 
+#   DESCRIPTION: Wrapper for command to ssh into docker container
 #
-#       OPTIONS: ---
-#  REQUIREMENTS: ---
-#          BUGS: ---
-#         NOTES: ---
-#        AUTHOR: YOUR NAME (), 
-#  ORGANIZATION: 
-#       CREATED: 12/05/2020 08:14:56 AM
-#      REVISION:  ---
 #===============================================================================
 
 set -o nounset                                  # Treat unset variables as an error
 
 docker exec -it first-java-webapp_webapp_1 /bin/bash
-
-# javac -cp /usr/local/tomcat/lib/servlet-api.jar classes src/Ch1Servlet.java

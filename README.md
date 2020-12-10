00c43148c5260acdc1cf84e237fc0995f7a1428a
Andy Osborne <caosborne.az@gmail.com>
2020-12-09 20:13:47 -0700

    Upgrade tomcat to version 10 and java to version 11. Replace deprecated javax library with jakarta.

---
+++ README.md
@@ -0,0 +1,5 @@
+# First Java Servlet
+
+
+
+When built using docker-compose the app will be located at http://localhost:8080/ch1/Serv1
--- app/src/dev/andyosborne/Ch1Servlet.java
+++ app/src/dev/andyosborne/Ch1Servlet.java
@@ -1,12 +1,16 @@
 package dev.andyosborne;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
 import java.io.*;
 import java.util.*;
 
+@WebServlet(urlPatterns={"/hello"})
 public class Ch1Servlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html");
+
         java.util.Date today = new java.util.Date();
         String todaysDate = today.toString();
 
